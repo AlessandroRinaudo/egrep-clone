161cb12be75313c50731034d0e1317875e176f09
Alessandro RINAUDO <alessandro.rinaudo@gmail.com>
2021-09-20 18:55:59 +0200

    removed inutil phrases

--- RegEx.java
+++ RegEx.java
@@ -22,34 +22,31 @@ public class RegEx {
 
   //MAIN
   public static void main(String arg[]) {
-    System.out.println("Welcome to Bogota, Mr. Thomas Anderson.");
     if (arg.length!=0) {
       regEx = arg[0];
     } else {
       Scanner scanner = new Scanner(System.in);
-      System.out.print("  >> Please enter a regEx: ");
+      System.out.print("\n>> Please enter a regEx: ");
       regEx = scanner.next();
     }
-    System.out.println("  >> Parsing regEx \""+regEx+"\".");
-    System.out.println("  >> ...");
+    System.out.println(">> Parsing regEx \""+regEx+"\".");
+    System.out.println(">> ...");
     
     if (regEx.length()<1) {
-      System.err.println("  >> ERROR: empty regEx.");
+      System.err.println(">> ERROR: empty regEx.");
     } else {
-      System.out.print("  >> ASCII codes: ["+(int)regEx.charAt(0));
+      System.out.print(">> ASCII codes: ["+(int)regEx.charAt(0));
       for (int i=1;i<regEx.length();i++) System.out.print(","+(int)regEx.charAt(i));
       System.out.println("].");
       try {
         RegExTree ret = parse();
-        System.out.println("  >> Tree result: "+ret.toString()+".");
+        System.out.println(">> Tree result: "+ret.toString()+"\n");
       } catch (Exception e) {
-        System.err.println("  >> ERROR: syntax error for regEx \""+regEx+"\".");
+        System.err.println(">> ERROR: syntax error for regEx \""+regEx+"\".");
       }
     }
 
-    System.out.println("  >> ...");
-    System.out.println("  >> Parsing completed.");
-    System.out.println("Goodbye Mr. Anderson.");
+ 
   }
 
   //FROM REGEX TO SYNTAX TREE
