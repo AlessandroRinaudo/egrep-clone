80c6d493cf8f3eec19f9cd5edf2115ba01edaf9f
Alessandro RINAUDO <alessandro.rinaudo@gmail.com>
2021-09-27 19:48:04 +0200

    created a new class determinisation

---
+++ Determinisation.java
@@ -0,0 +1,17 @@
+import java.util.Map;
+import java.util.ArrayList;
+
+public class Determinisation {
+
+  protected Map<ArrayList<Integer>, Integer>[] FromNdfaToDfa;
+
+  public Determinisation(Map<ArrayList<Integer>, Integer>[] FromNdfaToDfa) {
+    this.FromNdfaToDfa = FromNdfaToDfa;
+  }
+
+  public static Determinisation step3Determinisation(step2_AhoUllman matriceEtape2) {
+
+    
+  }
+
+}
