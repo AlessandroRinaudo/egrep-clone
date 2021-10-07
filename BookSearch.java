bc3c6636e4408e1015a15c379960c17a6f25fbb0
Alessandro RINAUDO <alessandro.rinaudo@gmail.com>
2021-10-07 19:12:14 +0200

    Fix Book parser bug

--- BookSearch.java
+++ BookSearch.java
@@ -5,7 +5,7 @@ public class BookSearch {
   public static String searchRegexInBook(ArrayList<String> livre, CompleteAutomaton automateDeterminise) {
     String res = "";
     for (int i = 0; i < livre.size(); i++) {
-      String[] word = livre.get(0).split(" ");
+      String[] word = livre.get(i).split(" ");
       for (int j = 0; j < word.length; j++) {
         if (CompleteAutomaton.regexValidator(word[j], automateDeterminise)) {
           res += livre.get(i) + "\n";
