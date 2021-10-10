5e368897688fcdbb60bc43d417683f080c59ba49
Alessandro RINAUDO <alessandro.rinaudo@gmail.com>
2021-10-10 17:27:34 +0200

    Changed tests

--- CompleteAutomaton.java
+++ CompleteAutomaton.java
@@ -44,67 +44,6 @@ public class CompleteAutomaton {
 
   public static boolean regexValidator(String wordToValidate, CompleteAutomaton automateDeterminise, String regEx) {
 
-    // basic algorihm structure
-    ArrayList<Automate> automateRegex = automateDeterminise.regexAutomaton;
-    ArrayList<Integer> initialLetters = automateDeterminise.initialLEtters;
-    ArrayList<Integer> finalLetters = automateDeterminise.finalLetters;
-
-    // word conversion into char list
-    ArrayList<Character> word = convertStringToCharArrayList(wordToValidate);
-    boolean isConcatenate = false;
-    for (int i = 0; i < word.size(); i++) {
-      if (!initialLetters.contains((int) word.get(i)))
-        continue;
-      if (finalLetters.contains((int) word.get(i)))
-        return true;
-      // int initialLine = 0;
-      // int initialColumn = (int) word.get(i);
-      // int j = i + 1;
-      // while (j != word.size() && !isConcatenate) {
-      // isConcatenate = concatenation(word.get(j), initialLine, initialColumn,
-      // finalLetters, automateRegex);
-      // initialLine += 1; // le numéro de la ligne qui suit
-      // initialColumn = word.get(j);
-      // j++;
-      // }
-      if (wordToValidate.contains(regEx)) {
-        return true;
-      }
-    }
-    return isConcatenate;
-  }
-
-  private static boolean concatenation(int character, int initialLine, int initialColumn,
-      ArrayList<Integer> finalLetters, ArrayList<Automate> automateRegex) {
-    ArrayList<Integer> newInitList = newInitialList(initialLine, initialColumn, automateRegex);
-    // System.out.println("initial line : "+initialLine);
-    if (!newInitList.contains(character))
-      return false;
-    if (newInitList.contains(character) && finalLetters.contains(character)) {
-      return true;
-    }
-
-    return false;
-  }
-
-  // new initial state list
-  private static ArrayList<Integer> newInitialList(int line, int column, ArrayList<Automate> automateRegex) {
-
-    ArrayList<Integer> res = new ArrayList<Integer>();
-    int findValue = -1;
-    for (int i = 0; i < automateRegex.size(); i++) {
-      if (automateRegex.get(i).line == line && automateRegex.get(i).column == column)
-        findValue = automateRegex.get(i).valeur;
-    }
-    for (int i = 0; i < automateRegex.size(); i++) {
-      if (automateRegex.get(i).line == findValue)
-        res.add(automateRegex.get(i).column);
-    }
-    return res;
-  }
-
-  public static boolean validatore(String wordToValidate, CompleteAutomaton automateDeterminise, String regEx) {
-
     ArrayList<Character> word = convertStringToCharArrayList(wordToValidate);
     int initialState = automateDeterminise.firstState;
     boolean found = false;
@@ -116,7 +55,6 @@ public class CompleteAutomaton {
     }
     if (!found)
       return false;
-    ///////////////////////////////////////////////////////////////
 
     found = false;
 
@@ -138,8 +76,6 @@ public class CompleteAutomaton {
 
       initialState = indexAutorizhedLetter((int) word.get(i), initialState, automateDeterminise);
       i++;
-      System.out.println("Stato iniziale : " + initialState);
-
     }
     return found;
   }
