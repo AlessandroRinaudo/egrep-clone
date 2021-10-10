7d975d7e90bf4252e765e47e8223d3ec12966477
Alessandro RINAUDO <alessandro.rinaudo@gmail.com>
2021-10-10 17:05:59 +0200

    Add validatorFunction

--- CompleteAutomaton.java
+++ CompleteAutomaton.java
@@ -6,11 +6,16 @@ public class CompleteAutomaton {
   protected ArrayList<Integer> initialLEtters;
   protected ArrayList<Integer> finalLetters;
 
+  protected int firstState;
+  protected ArrayList<Integer> finalState;
+
   public CompleteAutomaton(ArrayList<Automate> regexAutomaton, ArrayList<Integer> initialLEtters,
-      ArrayList<Integer> finalLetters) {
+      ArrayList<Integer> finalLetters, ArrayList<Integer> finalState) {
     this.regexAutomaton = regexAutomaton;
     this.initialLEtters = initialLEtters;
     this.finalLetters = finalLetters;
+    firstState = 0;
+    this.finalState = finalState;
   }
 
   @Override
@@ -19,8 +24,10 @@ public class CompleteAutomaton {
     for (int i = 0; i < regexAutomaton.size(); i++) {
       res += regexAutomaton.get(i);
     }
-    res += "\n initial letters : " + initialLEtters;
-    res += "\n final letters : " + finalLetters;
+    // res += "\n initial letters : " + initialLEtters;
+    // res += "\n final letters : " + finalLetters;
+    res += "\n initial states : " + firstState;
+    res += "\n final states : " + finalState;
     res += "\n\nEND AUTOMATE PROPRE\n";
     return res;
   }
@@ -35,7 +42,7 @@ public class CompleteAutomaton {
     return chars;
   }
 
-  public static boolean regexValidator(String wordToValidate, CompleteAutomaton automateDeterminise,String regEx) {
+  public static boolean regexValidator(String wordToValidate, CompleteAutomaton automateDeterminise, String regEx) {
 
     // basic algorihm structure
     ArrayList<Automate> automateRegex = automateDeterminise.regexAutomaton;
@@ -54,10 +61,11 @@ public class CompleteAutomaton {
       // int initialColumn = (int) word.get(i);
       // int j = i + 1;
       // while (j != word.size() && !isConcatenate) {
-      //   isConcatenate = concatenation(word.get(j), initialLine, initialColumn, finalLetters, automateRegex);
-      //   initialLine += 1; // le numéro de la ligne qui suit
-      //   initialColumn = word.get(j);
-      //   j++;
+      // isConcatenate = concatenation(word.get(j), initialLine, initialColumn,
+      // finalLetters, automateRegex);
+      // initialLine += 1; // le numéro de la ligne qui suit
+      // initialColumn = word.get(j);
+      // j++;
       // }
       if (wordToValidate.contains(regEx)) {
         return true;
@@ -67,7 +75,7 @@ public class CompleteAutomaton {
   }
 
   private static boolean concatenation(int character, int initialLine, int initialColumn,
-    ArrayList<Integer> finalLetters, ArrayList<Automate> automateRegex) {
+      ArrayList<Integer> finalLetters, ArrayList<Automate> automateRegex) {
     ArrayList<Integer> newInitList = newInitialList(initialLine, initialColumn, automateRegex);
     // System.out.println("initial line : "+initialLine);
     if (!newInitList.contains(character))
@@ -75,7 +83,7 @@ public class CompleteAutomaton {
     if (newInitList.contains(character) && finalLetters.contains(character)) {
       return true;
     }
-      
+
     return false;
   }
 
@@ -95,10 +103,83 @@ public class CompleteAutomaton {
     return res;
   }
 
-  // private static boolean newConcatenation () {
+  public static boolean validatore(String wordToValidate, CompleteAutomaton automateDeterminise, String regEx) {
 
-  // }
+    ArrayList<Character> word = convertStringToCharArrayList(wordToValidate);
+    int initialState = automateDeterminise.firstState;
+    boolean found = false;
 
+    // Check if any letter are not present in automate
+    for (int i = 0; i < word.size() && !found; i++) {
+      if (findLetterInAutomaton((int) word.get(i), automateDeterminise))
+        found = true;
+    }
+    if (!found)
+      return false;
+    ///////////////////////////////////////////////////////////////
 
+    found = false;
+
+    int i = 0;
+    while (!found && !word.isEmpty() && i < word.size()) {
+      if (!findLetterInAutomaton((int) word.get(i), automateDeterminise)) {
+        word.remove(i);
+        continue;
+      }
+      // check if the letter is the fist of occurences
+      if (!isAutorizhedLetter((int) word.get(i), initialState, automateDeterminise)) {
+        word.remove(i);
+        continue;
+      }
+      // check if the letter is also the final state
+      if (isFinalLetter((int) word.get(i), initialState, automateDeterminise)) {
+        found = true;
+      }
+
+      initialState = indexAutorizhedLetter((int) word.get(i), initialState, automateDeterminise);
+      i++;
+      System.out.println("Stato iniziale : " + initialState);
+
+    }
+    return found;
+  }
+
+  private static boolean findLetterInAutomaton(int letter, CompleteAutomaton automataton) {
+    ArrayList<Automate> automatonList = automataton.regexAutomaton;
+    for (Automate automatonOccurence : automatonList) {
+      if (automatonOccurence.column == letter)
+        return true;
+    }
+    return false;
+  }
+
+  private static boolean isAutorizhedLetter(int letterToFind, int initialState, CompleteAutomaton automataton) {
+    ArrayList<Automate> automatonList = automataton.regexAutomaton;
+    for (Automate automatonOccurence : automatonList) {
+      if (automatonOccurence.column == letterToFind && automatonOccurence.line == initialState)
+        return true;
+    }
+    return false;
+  }
+
+  private static int indexAutorizhedLetter(int letterToFind, int initialState, CompleteAutomaton automataton) {
+    ArrayList<Automate> automatonList = automataton.regexAutomaton;
+    for (int i = 0; i < automatonList.size(); i++) {
+      if (automatonList.get(i).column == letterToFind && automatonList.get(i).line == initialState)
+        return automatonList.get(i).valeur;
+    }
+    return -1;
+  }
+
+  private static boolean isFinalLetter(int letterToFind, int initialState, CompleteAutomaton automataton) {
+    ArrayList<Integer> finalState = automataton.finalState;
+    ArrayList<Automate> automatonList = automataton.regexAutomaton;
+    for (Automate automatonOccurence : automatonList) {
+      if (automatonOccurence.column == letterToFind && automatonOccurence.line == initialState
+          && finalState.contains(automatonOccurence.valeur))
+        return true;
+    }
+    return false;
+  }
 
 }
